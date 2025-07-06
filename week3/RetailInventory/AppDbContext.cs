a63ed6b4e131e1dbde50f9673c2c4cc9e2ea6f96
Piyush Prasun <146322550+PiyushPrasun@users.noreply.github.com>
2025-07-06 14:18:27 +0000

    added week3 work

---
+++ week3/RetailInventory/AppDbContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using RetailInventory.Models;
+
+namespace RetailInventory
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=yourserver;Database=yourdb;User Id=yourusername;Password=yourpassword;TrustServerCertificate=True;");
+        }
+    }
+}
+
